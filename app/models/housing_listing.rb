3771580e2efce8d4cfcf0c84a6d150463954f875
aliyeysides <ali.yeysides@gmail.com>
2015-01-31 14:18:17 -0500

    add validations to housing listing model

--- app/models/housing_listing.rb
+++ app/models/housing_listing.rb
@@ -1,2 +1,6 @@
 class HousingListing < ActiveRecord::Base
+	validates :room_sharing, presence: true
+	validates :price, presence: true
+	validates :location, presence: true, uniqueness: true
+	validates :max_avail, presence: true
 end
