b3f2c5b8e1f74984906df48d67aa6c3791ceec8d
aliyeysides <ali.yeysides@gmail.com>
2015-01-31 14:22:36 -0500

    add has_many relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	
+	has_many :housing_listings
+
 	validates :full_name, presence: true
 	validates :email, presence: true, uniqueness: true
 end
