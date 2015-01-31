9082df87400250d605955b1518a7a8d5cb0a8ba9
hilarybarr <hilaryrosebarr@gmail.com>
2015-01-31 17:44:32 -0500

    Styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @current_user = User.find(session[:user_id])
+    #@current_user = User.find(session[:user_id])
   end
 
   def new
@@ -11,20 +11,23 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "You have signed up successfully."
       redirect_to housing_listings_path
     else
-      render :new
+      redirect_to root_path
     end
+  end
+
+  def destroy
+    @user = User.find(user_params)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:full_name, :email, :facebook_url)
+    params.require(:user).permit(:full_name, :email, :facebook_url, :gender)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
 	has_many :housing_listings
 	has_many :roommate_listings
 
-	validates :full_name, presence: true
-	validates :email, presence: true, uniqueness: true
+	# validates :full_name, presence: true
+	# validates :email, presence: true, uniqueness: true
 end
