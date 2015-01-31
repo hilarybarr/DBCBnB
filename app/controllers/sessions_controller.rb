451ca28ce45dc753231e1269bbfd4f7d99b19e58
aliyeysides <ali.yeysides@gmail.com>
2015-01-31 13:16:00 -0500

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+	def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      redirect_to(root_path)
+    end
+  end
+
+  def create
+    user = User.where(username: params[:signin][:username]).first
+
+    if user && user.authenticate(params[:signin][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Signed in successfully."
+      # redirect_to some_path
+    else
+      flash[:notice] = "Sorry, those credentials are incorrect."
+      redirect_to signin_path
+    end
+  end
 end
