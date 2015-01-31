27aa43b2e53e813d4ccc5a1999fb96ff86b842e7
aliyeysides <ali.yeysides@gmail.com>
2015-01-31 14:00:34 -0500

    add create method in housing listing controller

--- app/controllers/housing_listings_controller.rb
+++ app/controllers/housing_listings_controller.rb
@@ -3,4 +3,17 @@ class HousingListingsController < ApplicationController
 		@listings = HousingListing.all
 	end
 
+	def new
+	end
+
+	def create
+		@listing = HouseListing.new()
+
+		if @listing.save
+			redirect housing_listing_path
+		else
+			session[:error] = @listing.errors.full_messages
+			redirect housing_listing_path
+		end
+	end
 end
