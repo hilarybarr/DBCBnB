c6f79949e44c088c32b66463546c028d1dfe0eec
aliyeysides <ali.yeysides@gmail.com>
2015-01-31 12:41:24 -0500

    add user and session routes and generate user controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  resources :users
+
+  get 'logout' => 'sessions#destroy'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
